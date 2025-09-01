37d4a527a20bf8f1ddbc2693cd5a427adcfdbf3b
alex <41588564+anguy079@users.noreply.github.com>
2025-09-01 06:50:51 -0700

    Update gen_hydrated_docs.py

--- scripts/gen_hydrated_docs.py
+++ scripts/gen_hydrated_docs.py
@@ -1,13 +1,7 @@
+from scripts import append_if_missing
 from pathlib import Path
-from mkdocs_gen_files import new_file
 
-countdown_snippet = """
-{{ COUNTDOWN_SNIPPET }}
-"""
+countdown_snippet = "{{ COUNTDOWN_SNIPPET }}"
 
 for path in Path("docs/tmp").rglob("*.md"):
-    content = path.read_text()
-    if "{{ COUNTDOWN_SNIPPET }}" not in content:
-        content += "\n\n" + countdown_snippet
-        with new_file(path) as f:
-            f.write(content)
+    append_if_missing(str(path), countdown_snippet)
