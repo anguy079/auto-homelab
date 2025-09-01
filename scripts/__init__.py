adae84fefc222ecf0bead2d96d25f466ce13aaa7
alex <41588564+anguy079@users.noreply.github.com>
2025-09-01 09:07:21 -0700

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from mkdocs_gen_files import new_file
+import mkdocs_gen_files
 
 def write_if_missing(path_str, content):
     path = Path(path_str)
     if not path.exists():
-        with new_file(path) as f:
+        with mkdocs_gen_files.open(path, "w") as f:
             f.write(content)
 
 def append_if_missing(path_str, snippet, marker="{{ COUNTDOWN_SNIPPET }}"):
@@ -13,5 +13,5 @@ def append_if_missing(path_str, snippet, marker="{{ COUNTDOWN_SNIPPET }}"):
         content = path.read_text()
         if marker not in content:
             content += "\n\n" + snippet
-            with new_file(path) as f:
+            with mkdocs_gen_files.open(path, "w") as f:
                 f.write(content)
