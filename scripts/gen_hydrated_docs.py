3baa39b2503bcdffe0a7f3cbcf2904cde45e93e5
alex <41588564+anguy079@users.noreply.github.com>
2025-09-01 19:54:12 -0700

    Delete scripts/gen_hydrated_docs.py

--- scripts/gen_hydrated_docs.py
+++
@@ -1,7 +0,0 @@
-from scripts import append_if_missing
-from pathlib import Path
-
-countdown_snippet = "{{ COUNTDOWN_SNIPPET }}"
-
-for path in Path("docs/tmp").rglob("*.md"):
-    append_if_missing(str(path), countdown_snippet)
