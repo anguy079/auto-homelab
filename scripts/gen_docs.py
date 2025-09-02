59837d5fbbcdbdb5b4e0209312f784bb770e2422
alex <41588564+anguy079@users.noreply.github.com>
2025-09-01 23:37:36 -0700

    Delete scripts/gen_docs.py

--- scripts/gen_docs.py
+++
@@ -1,4 +0,0 @@
-from scripts import write_if_missing
-
-# This script no longer creates index.md
-write_if_missing("docs/mkdocs/mkdocs-overview.md", "# 📦 MkDocs Overview\n\nFeatures, plugins, and setup.\n")
